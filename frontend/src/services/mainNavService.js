0bce955dc99ed9e8430691118c23f7a8685f39e7
evikk <evyatarm@gmail.com>
2021-01-18 12:25:38 +0200

    nav bar fix

--- frontend/src/services/mainNavService.js
+++ frontend/src/services/mainNavService.js
@@ -7,11 +7,17 @@ function navPos() {
     
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
+        
+        if (prevScrollpos > currentScrollPos && currentScrollPos > 0) {
+            document.querySelector('.navbar').style.top = "0";
+            document.querySelector('.navbar').style.backgroundColor ="#ffffff";
+        } 
+        else if (currentScrollPos === 0) {
             document.querySelector('.navbar').style.top = "0";
             document.querySelector('.navbar').style.backgroundColor ="transparent";
-        } else {
-            document.querySelector('.navbar').style.top = "-50px";
+        }
+        else {
+            document.querySelector('.navbar').style.top = "-70px";
             document.querySelector('.navbar').style.backgroundColor ="#ffffff";
         }
         prevScrollpos = currentScrollPos;
