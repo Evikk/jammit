accfbeb2f554448abeff000471420c75fc7355e8
Daniel Levy <Daniel450Ld@gmail.com>
2021-01-18 12:03:07 +0200

    not

--- frontend/src/services/mainNavService.js
+++ frontend/src/services/mainNavService.js
@@ -9,6 +9,7 @@ function navPos() {
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             document.querySelector('.navbar').style.top = "0";
+            document.querySelector('.navbar').style.backgroundColor ="transparent";
         } else {
             document.querySelector('.navbar').style.top = "-50px";
             document.querySelector('.navbar').style.backgroundColor ="#ffffff";
