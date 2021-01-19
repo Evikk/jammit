26195cd22962df91203e12cbc30172d871dfb951
evikk <evyatarm@gmail.com>
2021-01-19 22:56:50 +0200

    before demo 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadJams } from '../store/actions/jamActions.js'
-import { loadUsers } from '../store/actions/userActions.js'
+import { loadUsers, login } from '../store/actions/userActions.js'
 import { UserList } from '../cmps/UserList.jsx'
 import { JamScroll } from '../cmps/JamScroll.jsx'
 import { HeroSection } from '../cmps/HeroSection.jsx'
@@ -9,31 +9,44 @@ import Loader from 'react-loader-spinner'
 
 class _Home extends Component {
   state = {
-    
+    upcomingJams: []
   }
   componentDidMount() {
     this.props.loadJams()
     this.props.loadUsers()
+    this.getUpcomingJams()
   }
-  
+
+  getUpcomingJams = ()=> {
+    const upcomingJams = this.props.jams.filter(jam => {
+      const weekFromNow = new Date().getTime()+86400000*7
+      return jam.startsAt > Date.now() && jam.startsAt < weekFromNow
+    })
+    this.setState({upcomingJams})
+  }
 
   render() {
     const { jams, users, loggedInUser } = this.props
-    if (jams.length === 0 || users.length === 0) 
-    {return <div className="loader main-content pos-relative">
-    <Loader type="Bars" color="#00475F" height={200} width={200} timeout={5000} />
-</div>}
+    const { upcomingJams } = this.state
+    if (jams.length === 0 || users.length === 0 || upcomingJams.length === 0) 
+      return  <div className="loader main-content pos-relative">
+                <Loader type="Bars" color="#00475F" height={200} width={200} timeout={5000} />
+              </div>
     return ( 
       <div className="home">
         <HeroSection/>
-        <main className="main-content flex column space-between">
+        <main className="main-content zebra-container flex column space-between">
           <div className="jams section">
               <h1>Most Popular Jams</h1>
               <JamScroll jams={jams} />
           </div>
           <div className="jams section">
-              <h1>Upcoming Jams</h1>
-              <JamScroll jams={jams} />
+              <h1>Upcoming This Week</h1>
+              <JamScroll jams={upcomingJams} />
+          </div>
+          <div className="jams section">
+              <h1>Jams</h1>
+              <JamScroll jams={upcomingJams} />
           </div>
 
           <div className="members-container section">
@@ -44,8 +57,6 @@ class _Home extends Component {
                 })}/>
             </div>
           </div>
-
-          
         </main>
       </div>
     )
